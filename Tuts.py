de623d8fa00c8b12d3d002b903c4b54195c715a5
phantom5 <rickrudra@gmail.com>
2018-09-09 09:11:30 +0530

    prediction and plotting

--- Tuts.py
+++ Tuts.py
@@ -4,6 +4,12 @@ import math
 import numpy as np
 from sklearn import preprocessing,cross_validation,svm
 from sklearn.linear_model import LinearRegression
+import datetime
+from matplotlib import style
+import matplotlib.pyplot as plt
+
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key='qTgCxDQQ8_TKMnzYPEVi'
 dataf = quandl.get("WIKI/GOOGL")
@@ -20,16 +26,41 @@ forecast_out=int(math.ceil(0.001*len(dataf)))
 print(str(forecast_out)+" days")
 
 dataf['label']=dataf[forecast_col].shift(-forecast_out)
-dataf.dropna(inplace=True)
+
 x_features=np.array(dataf.drop(['label'],1))
-y_labels=np.array(dataf['label'])
+
+
 
 x_features=preprocessing.scale(x_features)
+x_features=x_features[:-forecast_out]
+x_lately=x_features[-forecast_out:]
+dataf.dropna(inplace=True)
+y_labels=np.array(dataf['label'])
 x_train,x_test,y_train,y_test=cross_validation.train_test_split(x_features,y_labels,test_size=0.2)
 
 clf=LinearRegression(n_jobs=-1) #run as many jobs as possible, fastens training
 clf.fit(x_train,y_train)
 accuracy=clf.score(x_test,y_test)
-print(accuracy*100)
+forecast_set=clf.predict(x_lately)
+print(forecast_set,accuracy,forecast_out)
+dataf['Forecast']=np.nan
+
+last_date=dataf.iloc[-1].name
+last_unix=last_date.timestamp()
+one_day=86400
+next_unix=last_unix+one_day
+
+for i in forecast_set:
+    next_date=datetime.datetime.fromtimestamp(next_unix)
+    next_unix+=one_day
+    dataf.loc[next_date]=[np.nan for _ in range(len(dataf.columns)-1)]+[i]
+
+dataf['Adj. Close'].plot()
+dataf['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
 
 
